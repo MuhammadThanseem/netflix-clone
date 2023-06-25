29e04a1f230b4c3b2a78be3567181d9365c7828c
MuhammadThanseem <thanseem@vuelogix.com>
2023-06-25 19:54:56 +0530

    App component css modified.

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <template>
-  <Navbar/>
-  <Banner/>
+  <Navbar />
+  <Banner />
 </template>
 <script>
-import Banner from './components/Banner/banner.vue'
-import Navbar from './components/Navbar/navbar.vue'
+import Banner from "./components/Banner/banner.vue";
+import Navbar from "./components/Navbar/navbar.vue";
 export default {
   components: { Navbar, Banner },
-  
-}
+};
 </script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  background-color: #111;
 }
 </style>
