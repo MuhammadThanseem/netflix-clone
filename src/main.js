246af6099a74d59d75e7184174612b3c2e85b8c1
MuhammadThanseem <thanseem@vuelogix.com>
2023-06-25 21:36:02 +0530

    vue youtube module imported

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueYoutube from 'vue-youtube'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+ 
+app.use('youtube',VueYoutube)
+
+app.use(router).mount('#app')
